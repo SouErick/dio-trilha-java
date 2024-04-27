dfb5119f00319bfd08ae9075b7705c341136fe9d
SouErick <ericksousasaraiva@gmail.com>
2024-04-27 17:24:27 -0300

    Exercicio ContaTerminal

---
+++ java-project/src/ContaTerminal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String [] args){
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("NOME DO TITULAR: ");
+        String nomeTitular = teclado.nextLine();
+        System.out.print("DEPÓSITO: R$ ");
+        double deposito = Double.parseDouble(teclado.nextLine());
+        System.out.print("CPF: ");
+        String cpf = teclado.nextLine();
+        System.out.println("---------- DADOS DA CONTA ----------");
+        System.out.println("NOME: " + nomeTitular);
+        System.out.println("DEPÓSITO: " + deposito);
+        System.out.println("CPF: " + cpf);
+        teclado.close();
+    }
+}
